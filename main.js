3881e5fff904f815e3f8af24b5f2c95a5ab5e1ff
redoxst <80003023+redoxst@users.noreply.github.com>
2021-03-03 16:05:43 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
-var swiper = new Swiper('.swiper-container', {
-
-  direction: 'vertical',
+var swiper = new Swiper(".swiper-container", {
+  direction: "vertical",
   mousewheelControl: false,
   mousewheel: false,
   slidesPerView: 1,
   keyboard: {
     enabled: true,
-    onlyInViewport: true,
-  },
+    onlyInViewport: true
+  }
   //freeMode: true,
   //freeModeSticky: true,
 });
 
-window.addEventListener('wheel', function (e) {
-	if (e.deltaY > 0) {
+window.addEventListener("wheel", function(e) {
+  if (e.deltaY > 0) {
     // down
-    swiper.slideNext()
+    swiper.slideNext();
   } else {
     // UP
-    swiper.slidePrev()
+    swiper.slidePrev();
   }
-})
+});
